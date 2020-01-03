c7c99c1a5716fdb96b435fd3a5ddc19ce3b9ef2b
aaqib <muhammadaaqib925@gmail.com>
2020-01-03 19:51:33 +0500

    Working

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
+# dor this i will first import the mysql connector my which i will connect my database by using python
+
 import mysql.connector
 
+# defining a variable which will carry the username and password 
+
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="aaqib",
-    passwd="Aaaqib"
-)
+	host="localhost",
+	user="aaqib",
+	passwd="Aaaqib"
+	)
+
+
+# this will show me the address where the user is located in memory
 
 print(mydb)
