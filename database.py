426d7f8d86fd2a0cb5225e7a93060268fd27a34b
aaqib <muhammadaaqib925@gmail.com>
2020-01-03 19:55:16 +0500

    Working

--- database.py
+++ database.py
@@ -5,12 +5,22 @@ import mysql.connector
 # defining a variable which will carry the username and password 
 
 mydb = mysql.connector.connect(
-	host="localhost",
-	user="aaqib",
-	passwd="Aaaqib"
-	)
-
+    host="localhost",
+    user="aaqib",
+    passwd="Aaaqib"
+)
 
 # this will show me the address where the user is located in memory
 
 print(mydb)
+
+# in mysql...we need a cursor for our work to be done and queries for the commands
+
+mycursor = mydb.cursor()
+
+# to create a database we execute a querry of CREATE DATABASE inside a cursor exceute function
+
+mycursor.exceute("CREATE DATABASE Firstdatabase")
+
+
+
