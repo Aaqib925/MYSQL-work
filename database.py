b912eaf6be9897ec5b67bc39402165521d816509
aaqib <muhammadaaqib925@gmail.com>
2020-01-03 22:27:04 +0500

    Working

--- database.py
+++ database.py
@@ -14,3 +14,7 @@
 # mydb = mysql.connector.connect(host="localhost", user="root", passwd="password", database="mydatabase")
 
 # since there is no error it means that database is created...
+
+# now i will run some queries for mydatabase...which is are follows
+
+# table query
